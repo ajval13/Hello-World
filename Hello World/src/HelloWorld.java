221a43ee47ecefe916d123f03559b1849de6eacc
alex.valdo <alex.valdo@MU-C1-07.mullen.pvt.k12.co.us>
2016-04-29 08:52:01 -0600

    Initial Commit

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld 
+    {
+	public static void main(String[] args) 
+		   {
+           System.out.println("Hello World!");
+           System.out.println("I Come In Peace");
+           System.out.println("Im A Mullen Student");
+	       } 
+    }  
